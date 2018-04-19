850b5ac908f0867e81521721f3b35f945690ff48
Julio Renner <julio.renner@hotmail.com>
2018-04-19 09:03:48 -0300

    add and update test of TokenHandler

--- analisadorLexico/tests/tokenHandler.js
+++ analisadorLexico/tests/tokenHandler.js
@@ -7,8 +7,8 @@ test('variaveis e identificadores', t => {
     let code1 = "int x = 7;";
     let code2 = "int y;";
 
-    t.equal(tokenInstance.generateTokens(code1), "[reserved_word,int][id,1][Equal_Op,=][num,7]");
-    t.equal(tokenInstance.generateTokens(code2), "[reserved_word,int][id,2]");
+    t.equal(tokenInstance.generateTokens(code1), "[reserved_word,int][id,1][equal,=][num,7][semicolon,;]");
+    t.equal(tokenInstance.generateTokens(code2), "[reserved_word,int][id,2][semicolon,;]");
 
     t.end();
 });
@@ -16,9 +16,9 @@ test('variaveis e identificadores', t => {
 test('constantes numéricas', t => {
     let tokenInstance = new TokenHandler();
 
-    let code = "int x = 7 + 25 * 52";
+    let code = "int x = 7 + 25 * 52;";
 
-    t.equal(tokenInstance.generateTokens(code), "[reserved_word,int][id,1][Equal_Op,=][num,7][Arith_Op,+][num,25][Arith_Op,*][num,52]");
+    t.equal(tokenInstance.generateTokens(code), "[reserved_word,int][id,1][equal,=][num,7][Arith_Op,+][num,25][Arith_Op,*][num,52][semicolon,;]");
 
     t.end();
 });
@@ -27,8 +27,7 @@ test('palavras reservadas', t => {
     let tokenInstance = new TokenHandler();
 
     let code = "if( x == 10 )";
-    console.log("teste 1");
-    t.equal(tokenInstance.generateTokens(code), "[reserved_word,if][id,1][Relational_Op,==][num,10]");
+    t.equal(tokenInstance.generateTokens(code), "[reserved_word,if][l_paren,(][id,1][Relational_Op,==][num,10][r_paren,)]");
 
     t.end();
 });
@@ -38,8 +37,7 @@ test('operadores relacionais', t => {
 
     let code = "while( x != 0)";
 
-    console.log("teste 2");
-    t.equal(tokenInstance.generateTokens(code), "[reserved_word,while][id,1][Relational_Op,!=][num,0]");
+    t.equal(tokenInstance.generateTokens(code), "[reserved_word,while][l_paren,(][id,1][Relational_Op,!=][num,0][r_paren,)]");
 
     t.end();
 });
@@ -49,7 +47,7 @@ test('numeros de ponto flutuante (números reais)', t => {
 
     let code = "int x = 7.15 - 2.13;";
 
-    t.equal(tokenInstance.generateTokens(code), "[reserved_word,int][id,1][Equal_Op,=][num,7.15][Arith_Op,-][num,2.13]");
+    t.equal(tokenInstance.generateTokens(code), "[reserved_word,int][id,1][equal,=][num,7.15][Arith_Op,-][num,2.13][semicolon,;]");
 
     t.end();
 });
@@ -61,11 +59,15 @@ test('remoção de espaços em branco e comentários', t => {
     let comment2 = "/* Comentário 2 */";
     let comment3 = "int x = 0;/* */";
     let comment4 = "int x = 0; //teste";
+    let comment5 = `/* teste oi ; , 
+                    eu sou ele
+                    teste som */`;
 
     t.equal(tokenInstance.generateTokens(comment1), "");
     t.equal(tokenInstance.generateTokens(comment2), "");
-    t.equal(tokenInstance.generateTokens(comment3), "[reserved_word,int][id,1][Equal_Op,=][num,0]");
-    t.equal(tokenInstance.generateTokens(comment4), "[reserved_word,int][id,1][Equal_Op,=][num,0]");
+    t.equal(tokenInstance.generateTokens(comment3), "[reserved_word,int][id,1][equal,=][num,0][semicolon,;]");
+    t.equal(tokenInstance.generateTokens(comment4), "[reserved_word,int][id,1][equal,=][num,0][semicolon,;]");
+    t.equal(tokenInstance.generateTokens(comment5), "");
 
     t.end();
 });
@@ -73,11 +75,12 @@ test('remoção de espaços em branco e comentários', t => {
 test('strings', t => {
     let tokenInstance = new TokenHandler();
 
-    let code = `String sexo = "masculino";`;
-    let code2 = `String teste = "sexo masculino";`
+    let code = `string sexo = "masculino";`;
+    let code2 = `string teste = "sexo masculino";`
     let code3 = `// " "`;
 
-    t.equal(tokenInstance.generateTokens(code), '[reserved_word,String][id,1][Equal_Op,=][string_literal,masculino]');
+    t.equal(tokenInstance.generateTokens(code), '[reserved_word,string][id,1][equal,=][string_literal,masculino][semicolon,;]');
+    t.equal(tokenInstance.generateTokens(code2), '[reserved_word,string][id,2][equal,=][string_literal,sexo][string_literal,masculino][semicolon,;]')
     t.equal(tokenInstance.generateTokens(code3), '');
 
     t.end();
@@ -88,7 +91,29 @@ test('operadores lógicos', t => {
 
     let code = `if(idade > 70 && sexo == "masculino")`;
 
-    t.equal(tokenInstance.generateTokens(code), `[reserved_word,if][id,1][Relational_Op,>][num,70][logic_op,&&][id,2][Relational_Op,==][string_literal,masculino]`);
+    t.equal(tokenInstance.generateTokens(code), `[reserved_word,if][l_paren,(][id,1][Relational_Op,>][num,70][logic_op,&&][id,2][Relational_Op,==][string_literal,masculino][r_paren,)]`);
 
     t.end();
 });
+
+test('parâmetro por referência', t => {
+    let tokenInstance = new TokenHandler();
+
+    let code = "int num = 1;"
+    let code1 = `scanf ("%d",&num);`;
+
+    t.equal(tokenInstance.generateTokens(code), `[reserved_word,int][id,1][equal,=][num,1][semicolon,;]`);
+    t.equal(tokenInstance.generateTokens(code1), `[id,2][l_paren,(][string_literal,%d][comma,,][reference,&][id,1][r_paren,)][semicolon,;]`);
+
+    t.end();
+});
+
+test('array declaration', t => {
+    let tokenInstance = new TokenHandler();
+
+    let code = "int v[] = {5, 10, 15, 3, 10, 76, 5, 13, 33, 45};"
+
+    t.equal(tokenInstance.generateTokens(code), "[reserved_word,int][id,1][equal,=][l_bracket,{][num,5][comma,,][num,10][comma,,][num,15][comma,,][num,3][comma,,][num,10][comma,,][num,76][comma,,][num,5][comma,,][num,13][comma,,][num,33][comma,,][num,45][r_bracket,}][semicolon,;]");
+
+    t.end()
+})
